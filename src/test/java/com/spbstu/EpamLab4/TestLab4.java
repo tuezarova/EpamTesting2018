6ca12c0fc534d5a1174cacc5dfc45dd1b931a485
tuezarova <tuezarova@gmail.com>
2018-06-16 13:18:12 +0300

    upd

--- src/test/java/com/spbstu/EpamLab4/TestLab4.java
+++ src/test/java/com/spbstu/EpamLab4/TestLab4.java
@@ -25,6 +25,7 @@ public class TestLab4 {
     public static void init(){
         homePageSelenide = new HomePageSelenide();
         differentElementsPages = new DifferentElementsPages();
+        homePageSelenide.open();
     }
     @BeforeSuite
     public void beforeSuite() {
@@ -32,7 +33,7 @@ public class TestLab4 {
         com.codeborne.selenide.Configuration.timeout = 6000;
         com.codeborne.selenide.Configuration.browser = "chrome";
         TestLab4.init();
-        homePageSelenide.open();
+
     }
 
     @Test
